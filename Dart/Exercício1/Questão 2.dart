ba104bf8c627f4597a22d359ddc018836bfef21b
Deydson Costa <deydsonassuncao20@gmail.com>
2023-07-25 20:11:16 -0300

    Atividade 1 Questão 2

--- Dart/Exercício1/Questão 2.dart
+++ Dart/Exercício1/Questão 2.dart
@@ -1,3 +1,17 @@
 void main() {
-  print("Hello World");
+  final String texto = "Minhas férias foram muito boas, mas seria mais melhor ter ido ver o filme do Pelé.";
+
+  List<String> textoSeparado = texto.split(" ");
+  int counterWords = textoSeparado.length;
+
+  if (textoSeparado.contains("férias") || textoSeparado.contains("Férias")) {
+    print("O texto contém a palavra 'férias'.");
+  } else {
+    print("O texto não contém a palavra 'férias'.");
+  }
+
+  String textoCorrigido = texto.replaceAll("mais melhor", "melhor");
+
+  print("Número de palavras da redação: $counterWords");
+  print("Texto corrigido: \n$textoCorrigido");
 }
